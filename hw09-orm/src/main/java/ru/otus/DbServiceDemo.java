50a303689c586658469fb6021d3c959097196562
sevudan <sevudan.mk@yandex.ru>
2019-12-18 10:01:22 +0300

    hw09 fix class DbServiceDemo

--- hw09-orm/src/main/java/ru/otus/DbServiceDemo.java
+++ hw09-orm/src/main/java/ru/otus/DbServiceDemo.java
@@ -1,8 +1,11 @@
 package ru.otus;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.otus.api.dao.AccauntDao;
 import ru.otus.api.dao.UserDao;
 import ru.otus.h2.CreateTableAccaunt;
@@ -21,6 +24,7 @@ import ru.otus.jdbc.sessionmanager.SessionManagerJdbc;
 import ru.otus.orm.CreateTable;
 
 public class DbServiceDemo {
+  private static Logger logger = LoggerFactory.getLogger(DbServiceDemo.class);
 
   public static void main(String[] args) throws Exception {
     DataSource dataSource = new DataSourceH2();
@@ -52,17 +56,36 @@ public class DbServiceDemo {
 
 
     long userRootId = dbServiceUser.saveUser(userRoot);
-    dbServiceUser.getUser(userRootId);
+    Optional<User> newUserRootRs = dbServiceUser.getUser(userRootId);
 
     long userFooId = dbServiceUser.saveUser(userFoo);
-    dbServiceUser.getUser(userFooId);
+    Optional<User> newUserFoo  = dbServiceUser.getUser(userFooId);
 
     long accauntId = dbServiceAccaunt.saveAccaunt(testAccaunt);
-    dbServiceAccaunt.getAccaunt(accauntId);
+    Optional<Accaunt> accauntRs = dbServiceAccaunt.getAccaunt(accauntId);
 
     userFoo.setName("TST");
     dbServiceUser.saveUser(userFoo);
-    dbServiceUser.getUser(userFooId);
+    Optional<User> userFooChanged  = dbServiceUser.getUser(userFooId);
 
+    newUserRootRs.ifPresentOrElse(
+            crUser -> logger.info("created user, name: {}", crUser.getName()),
+            () -> logger.info("user was not created")
+    );
+
+    newUserFoo.ifPresentOrElse(
+            crUser -> logger.info("created user, name: {}", crUser.getName()),
+            () -> logger.info("user was not created")
+    );
+
+    userFooChanged.ifPresentOrElse(
+            crUser -> logger.info("user name changed, new name: {}", crUser.getName()),
+            () -> logger.info("user was not created")
+    );
+
+    accauntRs.ifPresentOrElse(
+            crAccaunt -> logger.info("created accaunt, type: {}", crAccaunt.getType()),
+            () -> logger.info("accaunt was not created")
+    );
   }
 }
